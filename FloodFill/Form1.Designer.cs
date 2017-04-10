92ede4f0517134183b859a858a398abf3cbb6c70
beisenbek.baisakov <beisenbek.baisakov@yandex.ru>
2017-04-10 12:50:25 +0600

    init

--- FloodFill/Form1.Designer.cs
+++ FloodFill/Form1.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(68, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 445);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(605, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 73);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Pen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(605, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 73);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Fill";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 582);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- FloodFill/Form1.cs
+++ FloodFill/Form1.cs
@@ -12,9 +12,121 @@ namespace FloodFill
 {
     public partial class Form1 : Form
     {
+        State state = State.Pen;
+
         public Form1()
         {
             InitializeComponent();
+            g = pictureBox1.CreateGraphics();
         }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            state = State.Pen;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            state = State.Fill;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            switch (state)
+            {
+                case State.Pen:
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        g.DrawLine(new Pen(Color.Black), prevLocation, e.Location);
+                        prevLocation = e.Location;
+                    }
+                    break;
+                case State.Fill:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        Point prevLocation;
+        Queue<Point> q = new Queue<Point>();
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            prevLocation = e.Location;
+            switch (state)
+            {
+                case State.Pen:
+                    break;
+                case State.Fill:
+                    q.Enqueue(e.Location);
+                    GoFloodFill();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private bool IsGoodPoint(Point p)
+        {
+            if (p.X < 0) return false;
+            if (p.Y < 0) return false;
+            if (p.X >= pictureBox1.Width) return false;
+            if (p.Y >= pictureBox1.Height) return false;
+
+
+            return true;
+        }
+        private void GoFloodFill()
+        {
+            while (q.Count > 0)
+            {
+                Point cur = q.Dequeue();
+
+                Fill(cur);
+
+                Point p1 = new Point(cur.X, cur.Y + 1);
+                Point p2 = new Point(cur.X + 1, cur.Y);
+                Point p3 = new Point(cur.X - 1, cur.Y);
+                Point p4 = new Point(cur.X, cur.Y - 1);
+
+                if (IsGoodPoint(p1))
+                {
+                    q.Enqueue(p1);
+                    GoFloodFill();
+                }
+                if (IsGoodPoint(p2))
+                {
+                    q.Enqueue(p2);
+                    GoFloodFill();
+                }\
+                if (IsGoodPoint(p3))
+                {
+                    q.Enqueue(p3);
+                    GoFloodFill();
+                }
+                if (IsGoodPoint(p4))
+                {
+                    q.Enqueue(p4);
+                    GoFloodFill();
+                }
+            }
+        }
+
+        Graphics g;
+
+        private void Fill(Point cur)
+        {
+         
+            g.FillRectangle(new Pen(Color.Black).Brush, cur.X,cur.Y, 1, 1);
+        }
+    }
+
+    enum State
+    {
+        Pen,
+        Fill
     }
 }
